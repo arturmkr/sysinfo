7b98b5ee48464554e1654349ec9fa52a1d004f51
amkrtychian <artur.mkr@gmail.com>
2023-04-22 21:55:42 +0300

    update

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from user_agents import parse
 
 app = FastAPI()
 
-@app.get("/track")
+@app.get("/info")
 async def track_user(request: Request):
     user_agent = request.headers.get("User-Agent")
     ip_address = request.client.host
     # Parse the user agent string to get the OS and browser information
     with open("user_data.txt", "a") as file:
         file.write(f"{ip_address},{user_agent}\n")
-    return RedirectResponse(url="https://www.google.com")
+    return RedirectResponse(url="https://craft-conf.com/2023/workshops")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
