d2f50f6fcd790e01b219b82f2a982737e09b6d21
amkrtychian <artur.mkr@gmail.com>
2023-04-23 12:34:47 +0300

    update

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Query
 from fastapi.responses import RedirectResponse
-from user_agents import parse
+import datetime
 
 app = FastAPI()
 
+
 @app.get("/info")
-async def track_user(request: Request):
+async def track_user(request: Request, ref_code: str = Query(None), dst: str = Query(None)):
     user_agent = request.headers.get("User-Agent")
     ip_address = request.client.host
-    # Parse the user agent string to get the OS and browser information
+    current_time = datetime.datetime.now()
     with open("user_data.txt", "a") as file:
-        file.write(f"{ip_address},{user_agent}\n")
-    return RedirectResponse(url="https://craft-conf.com/2023/workshops")
+        file.write(
+            f"time:{current_time.time()}, ref_code:{ref_code}, dst:{dst}, ip:{ip_address}, agent:{user_agent}\n")
+    return RedirectResponse(url=dst)
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=80)
